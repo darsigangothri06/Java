05dd57febd965f7d04b347e06db57c95009ad3e1
darsigangothri06 <darsigangothri@gmail.com>
2021-11-17 08:15:56 +0530

    Add: Command Line Arguments

---
+++ Basics/CommandLineArgs.java
@@ -0,0 +1,14 @@
+class CommandLineArgs
+{
+	public static void main(String a[])
+	{
+		/*
+		System.out.println(a[0]);
+		System.out.println(a[1]);
+		*/
+		int x = Integer.parseInt(a[0]);
+		int y = Integer.parseInt(a[1]);
+		System.out.println(x);
+		System.out.println(y);
+	}
+}
