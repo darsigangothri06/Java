e4a974792620f93264bc82757ffaacac76f9365d
darsigangothri06 <darsigangothri@gmail.com>
2021-11-22 21:29:24 +0530

    Add: Packages

---
+++ Basics/A.java
@@ -0,0 +1,9 @@
+package MyFirstPackage;
+
+public class A
+{
+	public void hello()
+	{
+		System.out.println("Hi, I'm A from MyFirstPackage");
+	}
+}
---
+++ Basics/PackSecond.java
@@ -0,0 +1,11 @@
+package MySecondPackage;
+import MyFirstPackage.*;
+
+class PackSecond
+{
+	public static void main(String args[])
+	{
+		A obj = new A();
+		obj.hello();
+	}
+}
