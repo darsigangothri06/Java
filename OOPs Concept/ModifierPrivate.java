ae598950965d3b1edd443291c47b163d76669801
darsigangothri06 <darsigangothri@gmail.com>
2021-11-30 16:39:06 +0530

    Add: Accessing data from Private Method

--- OOPs Concept/ModifierPrivate.java
+++ OOPs Concept/ModifierPrivate.java
@@ -1,3 +1,9 @@
+/*
+
+We cannot access a private variable/method in other class.
+We have to create a public class and call the private method inside it.
+Then, we can get our output.
+
 class B
 {
 	private int a = 10;
@@ -14,5 +20,24 @@ class ModifiersPrivate
 		B obj = new B();
 		System.out.println(obj.a);
 		obj.msg();
+	}
+}
+
+*/
+
+class PRIVATE {
+	private void display() {
+		System.out.println("Hi, I'm Private method.");
+	}
+
+	public void Call() {
+		display();
+	}
+}
+
+class ModifierPrivate {
+	public static void main(String args[]) {
+		PRIVATE obj = new PRIVATE();
+		obj.Call();
 	}
 }
