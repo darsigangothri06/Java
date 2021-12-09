3879734c5b880d0f3aafe53a42d263a8dfee48ae
darsigangothri06 <darsigangothri@gmail.com>
2021-12-09 18:01:34 +0530

    Add: Executing Commands using UNICODE

---
+++ ExecutingComments.java
@@ -0,0 +1,7 @@
+public class ExecutingComments {
+    public static void main(String args[]) {
+
+        // \u000d
+        System.out.print("I'm Comment But, I'm visible to you!! Hahahah");
+    }
+}
