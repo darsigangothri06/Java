2195bc54760b951e6f7aae615a7dbc5b8da6c4ff
darsigangothri06 <darsigangothri@gmail.com>
2021-12-28 20:53:43 +0530

    Add: Documentation Comments

---
+++ Basics/Doc.java
@@ -0,0 +1,10 @@
+public class Doc {
+    /**
+     * This is a documentation method for my class
+     */
+    public static void main(String args[]) {
+        /**
+         * This is a documentation comment for my main method.
+         */
+    }
+}
