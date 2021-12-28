dcbad3188762dd2f550937550df8d1336ae27bec
darsigangothri06 <darsigangothri@gmail.com>
2021-12-28 20:29:46 +0530

    Add: JOptionPane Input

---
+++ Basics/JPane.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+class JPane {
+    public static void main(String args[]) {
+        String name;
+        name = JOptionPane.showInputDialog("Hello: ");
+        System.out.println(name);
+    }
+}
