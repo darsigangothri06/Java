1c34e1ad8160a1daff175fb676760c9196fda6b9
darsigangothri06 <darsigangothri@gmail.com>
2022-02-02 16:45:16 +0530

    Add: Multiple Catch Block

--- Exceptions/MultipleCatch.java
+++ Exceptions/MultipleCatch.java
@@ -1,14 +1,13 @@
 public class MultipleCatch {
     public static void main(String args[]) {
         try {
-            String s = null;
-            int a = 0;
-            String str = "gangothri";
-            System.out.println(s.charAt(0));
-            System.out.println(3 / a);
+            int[] a = new int[5];
+            a[5] = 34;
         } catch (NullPointerException NPE) {
             System.out.println(NPE);
         } catch (ArithmeticException e) { // this is not going to execute
+            System.out.println(e);
+        } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println(e);
         }
     }
