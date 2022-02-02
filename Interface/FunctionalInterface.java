f7b9fbf3e63371e3cb1e45a792f40481c58930c0
darsigangothri06 <darsigangothri@gmail.com>
2022-02-03 04:32:35 +0530

    Add: Functional Interface

---
+++ Interface/FunctionalInterface.java
@@ -0,0 +1,12 @@
+interface Square {
+    int calculate(int x);
+}
+
+public class FunctionalInterface {
+    public static void main(String args[]) {
+        int a = 5;
+        Square s = (int x) -> x * x;
+        int ans = s.calculate(a);
+        System.out.println(ans);
+    }
+}
