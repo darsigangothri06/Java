da62f6a62753db8dde7cd94b8d35dc555b36a407
darsigangothri06 <darsigangothri@gmail.com>
2022-02-05 20:56:44 +0530

    Add: Strings

---
+++ Strings/CharSeq.java
@@ -0,0 +1,7 @@
+public class CharSeq {
+    public static void main(String args[]) {
+        char[] ch = { 'g', 'a', 'n', 'g', 'o', 't', 'h', 'r', 'i' };
+        String str = new String(ch);
+        System.out.println(str);
+    }
+}
