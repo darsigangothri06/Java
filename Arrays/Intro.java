b917c79fc33954af58843f904299d65d3a8e850a
darsigangothri06 <darsigangothri@gmail.com>
2022-02-24 08:24:42 +0530

    Add: Arrays of Varying size

--- Arrays/Intro.java
+++ Arrays/Intro.java
@@ -4,6 +4,18 @@ public class Intro {
         int b[] = new int[5]; // An integer array with size 5
         System.out.println(a[2]); // 3
         System.out.println(b[2]); // 0
+
+        // Dynamically changing the size of an array
+        a = new int[10];
+        a[9] = 12;
+        System.out.println(a[9]);
+
+        // Arrays of Varying Size
+        int[][] mat = new int[4][];
+        mat[0] = new int[1];
+        mat[1] = new int[2];
+        mat[2] = new int[3];
+        mat[3] = new int[4];
     }
 
 }
